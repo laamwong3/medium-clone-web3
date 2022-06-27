3bf1946b6194de2cb510fe8c689a810791a0e668
unknown <laamwong3@gmail.com>
2022-06-27 17:45:27 +0800

    add safemint functionality

--- smart-contract/contracts/Medium.sol
+++ smart-contract/contracts/Medium.sol
@@ -6,16 +6,27 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
+error Medium__Not_enough_money();
+error Medium__Fail_to_transfer_ether();
+
 contract Medium is ERC721, ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
-    Counters.Counter private _tokenIdCounter;
-    uint256 public fees;
+    Counters.Counter private tokenIdCounter;
+    uint256 private fees;
 
     constructor(
         string memory _name,
         string memory _symbol,
         uint256 _fees
-    ) ERC721(_name, _symbol) {}
+    ) ERC721(_name, _symbol) {
+        fees = _fees;
+    }
+
+    function safeMint(address _to, string calldata _uri) external payable {
+        if (msg.value < fees) revert Medium__Not_enough_money();
+        (bool success, ) = payable(owner()).call{value: fees}("");
+        if (!success) revert Medium__Fail_to_transfer_ether();
+    }
 
     function _burn(uint256 tokenId)
         internal
