93a71da0cf56de3f1c0536d360b4635cf903c644
unknown <laamwong3@gmail.com>
2022-06-28 17:11:04 +0800

    create abi for front-end

--- smart-contract/hardhat.config.ts
+++ smart-contract/hardhat.config.ts
@@ -23,6 +23,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
+  // defaultNetwork: "hardhat",
   solidity: "0.8.7",
   networks: {
     mumbai: {
--- smart-contract/scripts/deploy.ts
+++ smart-contract/scripts/deploy.ts
@@ -1,5 +1,6 @@
 import { ethers } from "hardhat";
 import dotenv from "dotenv";
+import fs from "fs";
 
 dotenv.config();
 
@@ -8,8 +9,17 @@ async function main() {
   const Medium = await ethers.getContractFactory("Medium");
   const medium = await Medium.deploy("Medium Blog", "BLOG", fees);
 
+  // console.log(medium.interface.format("json"));
   // await medium.deployed();
+  const mediumData = {
+    address: medium.address,
+    abi: JSON.parse(medium.interface.format("json").toString()),
+  };
 
+  fs.writeFileSync(
+    "../constants/contractABI/Medium.json",
+    JSON.stringify(mediumData, null, 2)
+  );
   console.log("Medium deployed to:", medium.address);
 }
 
